58bbf4010368645a9849dc27c0150ba17ca519aa
lliriK <lliriK0120@gmail.com>
2023-11-05 15:56:51 +0400

    Сделали 0 этап

--- ping-pong.py
+++ ping-pong.py
@@ -1,1 +1,15 @@
-import pygame
+from pygame import *
+
+
+window = display.set_mode((700, 500))
+display.set_caption('ping_pong')
+background = (200,255,255)
+window.fill(background)
+
+game = True
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
