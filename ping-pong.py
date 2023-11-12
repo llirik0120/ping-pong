1ed8d8871dbb6d771b707e9f1efd3b0ebb5a576a
lliriK <lliriK0120@gmail.com>
2023-11-12 15:44:04 +0400

    Сделал мячик

--- ping-pong.py
+++ ping-pong.py
@@ -17,11 +17,15 @@ class Racket(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
+        if keys[K_DOWN] and self.rect.y < 500 - 80:
             self.rect.y += self.speed
 
-    def update_1(self):
-        keys = key
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
 
 window = display.set_mode((700, 500))
 display.set_caption('ping_pong')
@@ -29,13 +33,39 @@ background = (200,255,255)
 window.fill(background)
 
 game = True
-
-racket1 = () 
-racket2 =
+clock = time.Clock()
+FPS = 60
+speed_x = 3
+speed_y = 3
+racket_l = Racket (15, 80, 'platform.png', 10, 200, 10)
+racket_r = Racket (15, 80, 'platform.png', 675, 200, 10)
+ball = GameSprite (50, 50, 'ball.png', 325, 225, 0)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+
+
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+
+    if ball.rect.y > 500 - 50 or ball.rect.y < 0:
+        speed_y *= -1
+
+    if sprite.collide_rect(ball, racket_l) or sprite.collide_rect(ball, racket_r):
+        speed_x *= -1
+
+
+
+    window.fill(background)
+    racket_l.update_l()
+    racket_l.reset()
+    racket_r.update_r()
+    racket_r.reset()
+    ball.reset()
+
     display.update()
+    clock.tick(FPS)
 
