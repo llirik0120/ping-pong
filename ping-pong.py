c001e97be2df5f17a80fa32d85d9b421c2dd0a4c
lliriK <lliriK0120@gmail.com>
2023-11-12 16:34:21 +0400

    всё сделал

--- ping-pong.py
+++ ping-pong.py
@@ -1,4 +1,5 @@
 from pygame import *
+font.init()
 
 class GameSprite(sprite.Sprite):
     def __init__(self, size_x, size_y, player_image, player_x, player_y, player_speed):
@@ -29,12 +30,16 @@ class Racket(GameSprite):
 
 window = display.set_mode((700, 500))
 display.set_caption('ping_pong')
-background = (200,255,255)
+background = (0,79,0)
 window.fill(background)
 
 game = True
+finish = False
+font_1 = font.Font(None, 35)
+lose1 = font_1.render('KТО ТО ЗДОХ', True, (180, 0, 0))
+lose2 = font_1.render('KТО ТО ЗДОХ 2', True ,(180, 0, 0))
 clock = time.Clock()
-FPS = 60
+FPS = 80
 speed_x = 3
 speed_y = 3
 racket_l = Racket (15, 80, 'platform.png', 10, 200, 10)
@@ -46,25 +51,34 @@ while game:
         if e.type == QUIT:
             game = False
 
+    
+    if not finish:
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y > 500 - 50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(ball, racket_l) or sprite.collide_rect(ball, racket_r):
+            speed_x *= -1
 
 
-    ball.rect.x += speed_x
-    ball.rect.y += speed_y
 
-    if ball.rect.y > 500 - 50 or ball.rect.y < 0:
-        speed_y *= -1
+        window.fill(background)
+        racket_l.update_l()
+        racket_l.reset()
+        racket_r.update_r()
+        racket_r.reset()
+        ball.reset()
 
-    if sprite.collide_rect(ball, racket_l) or sprite.collide_rect(ball, racket_r):
-        speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
 
-
-
-    window.fill(background)
-    racket_l.update_l()
-    racket_l.reset()
-    racket_r.update_r()
-    racket_r.reset()
-    ball.reset()
+        if ball.rect.x> 650:
+            finish = True
+            window.blit(lose2, (200, 200))
 
     display.update()
     clock.tick(FPS)
